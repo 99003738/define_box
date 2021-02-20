42738caf9d720df216696cfa38c36df7aad1a3eb
99003731 <shivanshu.shukla@ltts.com>
2021-02-20 11:39:12 +0530

    structure and input for different boxes added

---
+++ Boxes.c
@@ -0,0 +1,36 @@
+# include"Boxes.h"
+#include<stdlib.h>
+
+Box* add_boxes(Box *box1,unsigned int number_of_boxes)
+{
+    if(box1==NULL)
+    {
+        printf("You can't add anymore boxes");
+        return ;
+    }
+    printf("\nEnter how many boxes you want to add :");
+    scanf("%u",&number_of_boxes);
+    for(int i=0;i<number_of_boxes;i++)
+    {
+        printf("\nEnter Unique ID for the BOX :");
+        scanf("%u",box1->unique_id);
+    
+         printf("\nEnter Length for the BOX : ");
+        scanf("%u",box1->length);
+    
+         printf("\nEnter Breadth for the BOX : ");
+        scanf("%u",box1->breadth);
+    
+        printf("\nEnter Height for the BOX : ");
+        scanf("%u",box1->height);
+
+        printf("\nEnter Color for the BOX : ");
+        scanf("%s",box1->color);
+
+        printf("\nEnter Height for the BOX : ");
+        scanf("%u",box1->weight);
+
+        box1++;
+    }
+    return box1;
+}
