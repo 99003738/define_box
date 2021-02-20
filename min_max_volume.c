30738584dbfa68b4af787150f6d43607580b2efe
99003731 <shivanshu.shukla@ltts.com>
2021-02-20 12:12:44 +0530

    files to calculate difference between maximum and minimum volume created

---
+++ min_max_volume.c
@@ -0,0 +1,52 @@
+# include"min_max_volume.h"
+# include"Boxes.h"
+# include<stdio.h>
+#include<stdlib.h>
+
+unsigned long volume_of_box(Box *box1)
+{
+    unsigned long volume;
+    volume= (box1->length)*(box1->breadth)*(box1->height);
+    return volume;
+}
+
+unsigned long min_volume(Box *box1,const unsigned int number_of_boxes)
+{
+    unsigned long minimum_volume=65536;
+    Box *temp_box=box1;
+    for(int i=0;i<number_of_boxes;i++)
+    { 
+        if(minimum_volume > volume(temp_box))
+        {
+            minimum_volume = volume(temp_box);
+            temp_box++;
+        }
+    }
+    return minimum_volume;
+}
+
+unsigned long max_volume(Box *box1,const unsigned int number_of_boxes)
+{
+    int maximum_volume=0;
+    Box *temp_box=box1;
+    for(int i=0;i<number_of_boxes;i++)
+    {
+        if(maximum_volume < volume(temp_box))
+        {
+            maximum_volume = volume(temp_box);
+            
+            temp_box++;
+        }
+    }
+    return maximum_volume;
+}
+
+unsigned long difference_min_max_volume(Box *box1,const unsigned int number_of_boxes)
+{
+    unsigned long minimum_volume=min_volume(box1,number_of_boxes);
+    unsigned long maximum_volume=max_volume(box1,number_of_boxes);
+
+    unsigned long difference_in_volume= maximum_volume-minimum_volume;
+
+    return difference_in_volume;
+}
---
+++ min_max_volume.h
@@ -0,0 +1,51 @@
+/**
+ * @file Boxes.h
+ * @author Shivanshu Shukla (shivanshu.shukla@ltts.com)
+ * @brief This file stores header for all the boxes
+ * @version 0.1
+ * @date 2021-02-20
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+# ifndef _MINMAXVOLUME_H_
+# define _MINMAXVOLUME_H_
+
+# include"Boxes.h"
+
+/**
+ * @brief calculates volume of box
+ * 
+ * @param box1 passes boxes
+ * @return unsigned long volume of a single box
+ */
+unsigned long volume_of_box(Box *box1);
+
+/**
+ * @brief Finds minimum volume of all the boxes
+ * 
+ * @param box1 passes all the boxes
+ * @return unsigned long min_volume of all the boxes
+ */
+
+unsigned long min_volume(Box *box1,const unsigned int number_of_boxes);
+/**
+ * @brief Finds maximum volume of all the boxes
+ * 
+ * @param box1 passes all the boxes
+ * @return unsigned long max_volume of all the boxes
+ */
+
+unsigned long max_volume(Box *box1,const unsigned int number_of_boxes);
+
+/**
+ * @brief Calculates difference between maximum and minimum volume of all the boxes
+ * 
+ * @param box1 passes all the boxes
+ * @return unsigned long difference between maximum volume and minimum volume of all the boxes calculated
+ */
+
+unsigned long difference_min_max_volume(Box *box1,const unsigned int number_of_boxes);
+
+# endif
