d6e929ea9081ede9c335c77c6d24e45bebf8b7c8
99003779 <78867425+99003779@users.noreply.github.com>
2021-02-21 18:41:36 +0530

    Neha Committed

---
+++ display.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"add_last.h"
+#include<stdlib.h>
+#include"display.h"
+
+void displayone(Box *arrayOfBoxes)
+{  
+        printf("Unique ID - %d", arrayOfBoxes->unique_id);
+        printf("\nLength - %d", arrayOfBoxes->length);
+        printf("\nBreadth - %d", arrayOfBoxes->breadth);
+        printf("\nHeight - %d", arrayOfBoxes->height);
+        printf("\nColor - %s", arrayOfBoxes->color);
+        printf("\nWeight - %d", arrayOfBoxes->weight);
+}
+void displayall(Box *arrayOfBoxes,int n)
+{
+    Box *boxcurr=arrayOfBoxes;
+    for(int i=0;i<n;i++)
+    {
+        displayone(boxcurr++);
+    }
+}
