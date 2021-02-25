18ed874b4cbbc36a52c0ac2f66c82e81f2bab5af
99003781 <78873487+99003781@users.noreply.github.com>
2021-02-25 17:22:06 +0530

    added header by aman

---
+++ Inc/countbox.h
@@ -0,0 +1,10 @@
+#ifndef __COUNTBOX_H
+#define __COUNTBOX_H
+
+#include"box.h"
+
+
+int count_box(int *bptr,int GivUniq_Id);
+
+
+#endif
---
+++ Inc/findbox.h
@@ -0,0 +1,10 @@
+#ifndef __FINDBOX_H
+#define __FINDBOX_H
+
+#include"box.h"
+
+
+int find_box(int *bptr,int GivUniq_Id);
+
+
+#endif
